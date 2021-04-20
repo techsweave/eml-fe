87ca6f3be444d0a5cd274a970bc46406816c2581
marcobarbaresco <marcobarbaresco96@gmail.com>
2021-04-20 15:09:14 +0200

    fix profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -4,18 +4,18 @@ export default function auth() {
     const [session, loading] = useSession();
     if (!session)
         return (
-            <Layout title="Profile page">
-                <p>User not athenticated</p><br />
+            <Layout title="Profile page - EmporioLambda">
+                <p>User not authenticated</p><br />
                 <button onClick={(e) => {
                     e.preventDefault()
                     signIn("cognito")
-                }}>SignIn</button>
+                }}>Sign-In</button>
             </Layout>
         )
     return (
-        <Layout title="Profile page">
-            <p>User athenticated with{session.user.emial}</p><br />
-            <button onClick={() => signOut()}>SignOut</button>
+        <Layout title="Profile page - EmporioLambda">
+            <p>User authenticated with the following email: {session.user.email}<br /></p>
+            <button onClick={() => signOut()}>Sign-Out</button>
         </Layout>
 
     )
