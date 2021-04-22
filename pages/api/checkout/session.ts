a2382431d6e1fa372adc73adc35d64cd63ba3ba5
marcobarbaresco <marcobarbaresco96@gmail.com>
2021-04-22 17:20:25 +0200

    change stripe keys

--- pages/api/checkout/session.ts
+++ pages/api/checkout/session.ts
@@ -9,11 +9,11 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     payment_method_types: ['card'],
     line_items: [
       {
-        price: 'price_1Ij1bSKu55jX77IkDFfE7GnM',
+        price: 'price_1Ij4BcF20K2KHUILT4B6IKEO',
         quantity
       },
       {
-        price: 'price_1Ij1cPKu55jX77IkZglGZ0aF',
+        price: 'price_1Ij4CsF20K2KHUILKulBqesI',
         quantity
       }
     ],
