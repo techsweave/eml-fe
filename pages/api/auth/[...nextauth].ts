c8c1ccdbd809da6639315bfcf12f1dfa306c8690
Nicolò Giaccone <giacco9722@outlook.it>
2021-04-23 16:05:15 +0200

    add env for cognito add yarn

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,7 +5,6 @@ export default NextAuth({
     providers: [
         Providers.Cognito({
             clientId: process.env.COGNITO_CLIENT_ID,
-            clientSecret: process.env.COGNITO_CLIENT_SECRET,
             domain: process.env.COGNITO_DOMAIN
         })
     ]
