cc388c936ca7022f8ba0cf5db31e84f44d8d0d39
Nicolò Giaccone <giacco9722@outlook.it>
2021-04-26 16:48:33 +0200

    Co-authored-by: TitoScutari <TitoScutari@users.noreply.github.com> Co-authored-by: marcobarbaresco <marcobarbaresco@users.noreply.github.com>

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,20 +1,23 @@
 import Layout from '../../components/Layout'
 import CartList from '../../components/CartList'
 import product from '../../types/product'
-import cart from '../../types/cart'
+// import cart from '../../types/cart'
 import { loadStripe } from '@stripe/stripe-js'
-
+import { getLambdaResult } from '../api/lib/lambda'
+import { GetServerSideProps } from 'next'
+import { useSession } from 'next-auth/client'
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!)
 
 const alltheprods: product[] = [{ id: 1, name: 'Product sample 1', description: 'Description of Product 1', price: 10, quantity: 20 },
-{ id: 2, name: 'Product sample 2', description: 'Description of Product 2', price: 1, quantity: 2 },
-{ id: 3, name: 'Product sample 3', description: 'Description of Product 3', price: 2000, quantity: 1 }]
+  { id: 2, name: 'Product sample 2', description: 'Description of Product 2', price: 1, quantity: 2 },
+  { id: 3, name: 'Product sample 3', description: 'Description of Product 3', price: 2000, quantity: 1 }]
 
 const carteg: cart = { ID: 100, total: 1000, products: alltheprods }
 
-export default function Cart() {
+export default function Cart ({ record }) {
+  const [session, loading] = useSession()
   /* const handleClick = async (event) => {
     // Get Stripe.js instance
     const stripe = await stripePromise
@@ -34,9 +37,20 @@ export default function Cart() {
   } */
   return (
     <Layout title="Cart page">
-      <CartList cart={carteg} />
-      <br />
-      {/* <button className="goToCheckout" onClick={handleClick}>Checkout</button> */}
+    {!session && (
+        <span>User not authenticated, please sign-in to acces the cart</span>
+    )
+}
+      {session && (<div><CartList cart={carteg} />
+       {/*  <br />
+        <button className="goToCheckout" onClick={handleClick}>Checkout</button> */}</div>)}
     </Layout>
   )
 }
+/* export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {
+      record: await (await getLambdaResult('cart/0')).props.response.data
+    } // will be passed to the page component as props
+  }
+} */
