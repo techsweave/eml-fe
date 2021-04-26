96c74c6d0d8d67e48e61b022bc9dd2bb4650b95c
Nicolò Giaccone <giacco9722@outlook.it>
2021-04-26 18:11:29 +0200

    add getcart lambda

--- pages/api/lib/lambda.ts
+++ pages/api/lib/lambda.ts
@@ -2,9 +2,5 @@ import { Fetcher } from './fetcher'
 
 export async function getLambdaResult (functionName: string) {
   const fetcher = new Fetcher(functionName)
-  return {
-    props: {
-      response: await fetcher.getJsonResult()
-    }
-  }
+  return fetcher.getJsonResult()
 }
--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -3,23 +3,26 @@ import CartList from '../../components/CartList'
 import product from '../../types/product'
 import cart from '../../types/cart'
 import { loadStripe } from '@stripe/stripe-js'
-
+import { getLambdaResult } from '../api/lib/lambda'
+import { GetServerSideProps } from 'next'
+import { useSession } from 'next-auth/client'
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!)
 
 const alltheprods: product[] = [{ id: 1, name: 'Product sample 1', description: 'Description of Product 1', price: 10, quantity: 20 },
-{ id: 2, name: 'Product sample 2', description: 'Description of Product 2', price: 1, quantity: 2 },
-{ id: 3, name: 'Product sample 3', description: 'Description of Product 3', price: 2000, quantity: 1 }]
+  { id: 2, name: 'Product sample 2', description: 'Description of Product 2', price: 1, quantity: 2 },
+  { id: 3, name: 'Product sample 3', description: 'Description of Product 3', price: 2000, quantity: 1 }]
 
 const carteg: cart = { ID: 100, total: 1000, products: alltheprods }
 
-export default function Cart() {
+export default function Cart ({ record }) {
+  const [session, loading] = useSession()
   /* const handleClick = async (event) => {
     // Get Stripe.js instance
     const stripe = await stripePromise
     // Call your backend to create the Checkout Session
-    const { sessionId } = await fetch('/api/checkout/session', {
+    const { sessionId } = await fetch('https://b4bheanrza.execute-api.eu-central-1.amazonaws.com/dev/checkout', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -32,11 +35,26 @@ export default function Cart() {
       sessionId
     })
   } */
+  console.log(record)
   return (
     <Layout title="Cart page">
-      <CartList cart={carteg} />
-      <br />
-      {/* <button className="goToCheckout" onClick={handleClick}>Checkout</button> */}
+    {!session && (
+        <span>User not authenticated, please sign-in to acces the cart</span>
+    )
+}
+      {session && (<div></div>
+      /*  */
+      /* <br />
+        <button className="goToCheckout" onClick={handleClick}>Checkout</button> */)}
     </Layout>
   )
+}
+export const getServerSideProps: GetServerSideProps = async () => {
+  let cartItems = (await getLambdaResult('cart')).data
+  console.log(cartItems)
+  return {
+    props: {
+      record: ''
+} // will be passed to the page component as props
+  }
 }
