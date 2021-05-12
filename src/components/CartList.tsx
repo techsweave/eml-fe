3b49aea8d85ace52e5f6f322c9e1c2cc0d777dc0
Urbani Simone <urb.simone@gmail.com>
2021-05-12 19:54:11 +0200

    Comment CartList componets

--- src/components/CartList.tsx
+++ src/components/CartList.tsx
@@ -1,14 +1,15 @@
 import CartItem from '@components/CartItem'
-import cart from '@models/cart'
+import Cart from '@models/cart'
 import product from '@models/product'
 import cartStyles from '@styles/Cart.module.css'
 
-const CartList = (prop: { cart: cart }) => {
+const CartList = (prop: { cart: Cart }) => {
     return (
-        <div className={cartStyles.cartGrid}>
-            {prop.cart.products.map((product: product) => (<CartItem product={product} key={product.id} />))}
-            <p className={cartStyles.total}>total: {prop.cart.total}</p>
-        </div>
+        <div></div>
+        // <div className={cartStyles.cartGrid}>
+        //     {prop.cart.product?.map((product: product) => (<CartItem product={product} key={product.id} />))}
+        //     <p className={cartStyles.total}>total: {prop.cart.total}</p>
+        // </div>
     )
 }
 
