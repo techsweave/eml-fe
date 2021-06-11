18b919a14fd326ea82fccef83e388caa2f978386
Nicolò Giaccone <giacco9722@outlook.it>
2021-06-11 14:15:51 +0200

    complete responsive header

--- src/components/product/ProductItem.tsx
+++ src/components/product/ProductItem.tsx
@@ -2,13 +2,13 @@
 import Link from 'next/link';
 import Product from '@models/product';
 import React from 'react';
-import { Heading, Text } from '@chakra-ui/layout';
+import { Text } from '@chakra-ui/layout';
 import { Box, Image, Stack } from '@chakra-ui/react';
 
 const ProductItem = (prop: { product: Product }) => {
   const { product } = prop;
   return (
-    <Box as="button" w={['100%', '100%', '20%', '20%']} ml={['0', '0', '5', '5']} mr={['0', '0', '5', '5']} mt={['10', '10', '0', '0']}>
+    <Box as="button" w={['100%', '100%', '20%', '20%']} ml={['0', '0', '10', '10']} mt={['10', '10', '5', '5']}>
       <Link href={{ pathname: '/products/detail/[id]', query: { id: product.id } }}>
         <Stack position='relative'>
           <Image src={product.image} alt={product.name} w="100%" h={['300px', '300px', '150px', '150px']} borderRadius="15px" fit="cover" />
