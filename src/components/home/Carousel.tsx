47a80ef0700a1b6842e74eb8ba31062373688f1a
Nicolò Giaccone <giacco9722@outlook.it>
2021-06-15 18:53:10 +0200

    fix minor error

--- src/components/home/Carousel.tsx
+++ src/components/home/Carousel.tsx
@@ -5,32 +5,37 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, {
   Navigation, Pagination, Controller,
 } from 'swiper';
+import { Heading } from '@chakra-ui/react';
 
 SwiperCore.use([Navigation, Pagination, Controller]);
 const Carousel = (prop: { product: Models.Tables.IProduct[] }) => {
   const { product } = prop;
   return (
-    <Swiper
-      id='main'
-      tag="section"
-      wrapperTag="ul"
-      navigation
-      spaceBetween={0}
-      slidesPerView={1}
-    >
-      {product.map((products) => (
-        <SwiperSlide
-          id={products.id}
-          tag="li"
-          style={{ listStyle: 'none' }}
-        >
-          <CarouselItem
-            product={products}
-            key={products.id}
-          />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div>
+      <Heading textAlign='center' m='5'>Offer products</Heading>
+      <Swiper
+        id='main'
+        tag="section"
+        wrapperTag="ul"
+        navigation
+        pagination={{ clickable: true }}
+        spaceBetween={0}
+        slidesPerView={1}
+      >
+        {product.map((products) => (
+          <SwiperSlide
+            id={products.id}
+            tag="li"
+            style={{ listStyle: 'none' }}
+          >
+            <CarouselItem
+              product={products}
+              key={products.id}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
 export default Carousel;
