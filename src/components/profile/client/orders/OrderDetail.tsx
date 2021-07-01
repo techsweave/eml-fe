71d345773ddacbb4872ae5b046ca0073944d24b1
marcobarbaresco <marcobarbaresco96@gmail.com>
2021-07-01 12:05:49 +0200

    fix order table resizing

--- src/components/profile/client/orders/OrderDetail.tsx
+++ src/components/profile/client/orders/OrderDetail.tsx
@@ -11,10 +11,17 @@ import {
   Text,
   Center,
   Button,
+  useMediaQuery,
 } from '@chakra-ui/react';
 import Link from 'next/link';
 import { getProductsData } from '@test/ProductMock';
 import { Models } from 'utilities-techsweave';
+
+function getTableSize() {
+  const [isSmallerThan600] = useMediaQuery('(max-width: 600px)');
+  if (isSmallerThan600) return 'sm';
+  return 'md';
+}
 
 const OrderDetail = (prop: {
   products: Models.Tables.IOrderedProduct[],
@@ -40,11 +47,11 @@ const OrderDetail = (prop: {
           </Text>
         </Center>
       </Box>
-      <Table variant="simple" display={['none', 'table', 'table', 'table']}>
+      <Table variant="simple" size={getTableSize()}>
         <Thead>
           <Tr>
             <Th>Name</Th>
-            <Th>Product ID</Th>
+            <Th display={['none', 'none', 'table-cell', 'table-cell']}>Product ID</Th>
             <Th>Price</Th>
             <Th>Quantity</Th>
             <Th>Total</Th>
@@ -56,7 +63,7 @@ const OrderDetail = (prop: {
             return (
               <Tr _hover={{ backgroundColor: 'blue.100' }}>
                 <Td><Link href={{ pathname: '/products/detail/[id]', query: { id: productData.id } }}>{productData.title}</Link></Td>
-                <Td>{productData.id}</Td>
+                <Td display={['none', 'none', 'table-cell', 'table-cell']}>{productData.id}</Td>
                 <Td>{productData.price}</Td>
                 <Td>{ orderData.quantity}</Td>
                 <Td>{orderData.quantity * productData.price}</Td>
@@ -64,29 +71,6 @@ const OrderDetail = (prop: {
             );
           })}
 
-        </Tbody>
-      </Table>
-      <Table variant="simple" display={['table', 'none', 'none', 'none']} size="sm">
-        <Thead>
-          <Tr>
-            <Th>Name</Th>
-            <Th>Price</Th>
-            <Th>Quantity</Th>
-            <Th>Total</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {products.map((orderData) => {
-            const productData = getProductsData(orderData.productId);
-            return (
-              <Tr _hover={{ backgroundColor: 'blue.100' }}>
-                <Td><Link href={{ pathname: '/products/detail/[id]', query: { id: productData.id } }}>{productData.title}</Link></Td>
-                <Td>{productData.price}</Td>
-                <Td>{ orderData.quantity}</Td>
-                <Td>{orderData.quantity * productData.price}</Td>
-              </Tr>
-            );
-          })}
         </Tbody>
       </Table>
       <Center><Button as="a" href="/profile/client/profileOrders" w={['full', 'full', '600px', '900px']} m="10">Back</Button></Center>
