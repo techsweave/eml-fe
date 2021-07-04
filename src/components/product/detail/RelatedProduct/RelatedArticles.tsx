4ed5b887c52e8fafb7ec342ec5bcf8e6ed6661b4
Nicolò Giaccone <giacco9722@outlook.it>
2021-07-04 11:56:13 +0200

    minor layout change

--- src/components/product/detail/RelatedProduct/RelatedArticles.tsx
+++ src/components/product/detail/RelatedProduct/RelatedArticles.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import {
-  Box, Grid, GridItem, Heading,
+  Box, Grid, GridItem, Heading, Text,
 } from '@chakra-ui/react';
 import RelatedItem from '@components/product/detail/RelatedProduct/RelatedItem';
 import { Models } from 'utilities-techsweave';
 
 const RelatedProducts = (prop: { product: Models.Tables.IProduct[] }) => {
   const { product } = prop;
+  console.log(product.length);
   return (
     <Box>
       <Heading textAlign='center' m='10'>Related Product</Heading>
+      {product.length === 0 && (<Text textAlign='center' fontSize='3xl'>There is no products corrisponding with this category</Text>)}
+      {product.length !== 0 && (
       <Grid templateColumns={['repeat(2, 1fr)', 'repeat(2, 1fr)', 'repeat(6, 1fr)', 'repeat(6, 1fr)']} gap={10} ml={['5', '5', '10', '10']} mr={['5', '5', '10', '10']}>
         {product.map((products) => (
           <GridItem
@@ -22,6 +25,7 @@ const RelatedProducts = (prop: { product: Models.Tables.IProduct[] }) => {
           </GridItem>
         ))}
       </Grid>
+      )}
     </Box>
   );
 };
