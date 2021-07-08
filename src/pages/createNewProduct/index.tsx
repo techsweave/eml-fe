7af765285360640672767933d30ea3032baf93e0
Nicolò Giaccone <giacco9722@outlook.it>
2021-07-08 18:39:35 +0200

    fix

--- src/pages/createNewProduct/index.tsx
+++ src/pages/createNewProduct/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import CreateNew from '@components/product/vendor/CreateNew';
 import Layout from '@components/Layout';
 
-export default function createNewProduct({ record }) {
+export default function createNewProduct() {
   return (
     <Layout title="Create-new-product">
       <CreateNew />
