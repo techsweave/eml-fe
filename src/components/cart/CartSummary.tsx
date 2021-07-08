b98fdc3786c45e8db5592d29afbe873df84cba51
Urbani Simone <urb.simone@gmail.com>
2021-07-08 17:17:02 +0200

    Centralized summary satate

--- src/components/cart/CartSummary.tsx
+++ src/components/cart/CartSummary.tsx
@@ -1,114 +1,27 @@
-import { Models, Services } from 'utilities-techsweave';
-import React, { useEffect, useState } from 'react';
+import { Models } from 'utilities-techsweave';
+import React from 'react';
 import {
-  Flex, Text, Heading, CircularProgress, Button,
+  Flex, Text, Heading, Button,
 } from '@chakra-ui/react';
-import { ConditionExpression } from '@aws/dynamodb-expressions';
-import showError from '@libs/showError';
-import { useSession } from 'next-auth/client';
 import { FaShoppingBag } from 'react-icons/fa';
 
-type IProduct = Models.Tables.IProduct;
 type ICart = Models.Tables.ICart;
-const initTotalPrice = [0, 0];
-const initLoading = true;
+type IProduct = Models.Tables.IProduct;
+type ICartItemDetail = ICart & Omit<IProduct, 'id'>
 
-const CartSummary = (props: { cart: Array<ICart> }) => {
-  const [state, setState] = useState(initTotalPrice);
-  const [error, setError] = useState<Error>();
-  const [loading, setLoading] = useState(initLoading);
-
-  const session = useSession()[0];
+const CartSummary = (props: { cart: Array<ICartItemDetail> }) => {
   const { cart } = props;
+  let totalNoDiscount = 0;
+  let total = 0;
 
-  const getTotalPrice = async (): Promise<[number, number]> => {
-    let products: Array<IProduct> = [];
-    let scanResult: Models.IMultipleDataBody<IProduct> = {
-      data: [],
-      count: 0,
-    };
-    const ids: Array<string> = [];
-    let total = 0;
-    let totalNoDiscount = 0;
-
-    cart.forEach((x) => {
-      if (!x.isChanged) {
-        ids.push(x.productId);
-      }
-    });
-
-    if (ids.length === 0) {
-      return [0, 0];
+  cart.forEach((x) => {
+    let price = x.price ? x.price : 0;
+    totalNoDiscount += price * x.quantity;
+    if (x?.discount) {
+      price -= ((price / 100) * x.discount!);
     }
-
-    const filter: ConditionExpression = {
-      type: 'Membership',
-      subject: 'id',
-      values: ids,
-    };
-
-    const productService = new Services.Products(
-      process.env.NEXT_PUBLIC_API_ID_PRODUCTS!,
-      process.env.NEXT_PUBLIC_API_REGION!,
-      process.env.NEXT_PUBLIC_API_STAGE!,
-      session?.accessToken as string,
-      session?.idToken as string,
-    );
-
-    do {
-      scanResult = await productService.scanAsync(
-        50,
-        scanResult?.lastEvaluatedKey?.id,
-        undefined,
-        undefined,
-        filter,
-      );
-      products = products.concat(scanResult.count ? scanResult.data : scanResult as any);
-    } while (scanResult?.lastEvaluatedKey);
-
-    products.forEach((product) => {
-      const cartItem = cart.find((x) => x.productId === product.id)!;
-      let price = product.price ? product.price : 0;
-      totalNoDiscount += price * cartItem.quantity;
-      if (product?.discount) {
-        price -= ((price / 100) * product.discount!);
-      }
-      total += price * cartItem.quantity;
-    });
-
-    return Promise.resolve([total, totalNoDiscount]);
-  };
-
-  useEffect(() => {
-    // Avoid infinte loop render -> useEffect -> setState -> render
-    if (!loading) return;
-    if (error) return;
-
-    getTotalPrice()
-      .then((data) => {
-        setState(data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        setError(err.error);
-        setLoading(false);
-      });
-  }, [state, setState, error, setError, loading, setLoading]);
-
-  // if (loading) {
-  //   return (
-  //     <Flex
-  //       justifyContent='center'
-  //     >
-  //       <CircularProgress
-  //         isIndeterminate
-  //         color='red.300'
-  //       />
-  //     </Flex>
-  //   );
-  // }
-
-  showError(error);
+    total += price * x.quantity;
+  });
 
   const goToChechOut = async () => {
     // TODO
@@ -130,7 +43,7 @@ const CartSummary = (props: { cart: Array<ICart> }) => {
       <Heading
         as='h3'
       >
-        {state[0]}
+        {total}
         €
       </Heading>
       <Text
@@ -138,7 +51,7 @@ const CartSummary = (props: { cart: Array<ICart> }) => {
         color='gray.500'
         fontSize='lg'
       >
-        {state[1]}
+        {totalNoDiscount}
         €
       </Text>
       <Text
@@ -147,8 +60,8 @@ const CartSummary = (props: { cart: Array<ICart> }) => {
       >
         {(Math.round(
           100 * 100 * (
-            (state[1] - state[0])
-            / state[1]),
+            (totalNoDiscount - total)
+            / totalNoDiscount),
         ) / 100)
           .toString()
           .replace('.', ',')
