626678e588b12de73a60208d78a91cc4f91a6064
Nicolò Giaccone <giacco9722@outlook.it>
2021-07-08 18:45:36 +0200

    fix

--- src/test/OrderMock.ts
+++ src/test/OrderMock.ts
@@ -5,8 +5,7 @@ const OrderMock: Models.Tables.IOrder[] = [
   {
     id: 'ordine1',
     userId: '',
-    // date: new Date('2021-06-10'),
-    date: '2021-06-10',
+    date: new Date('2021-06-10'),
     status: '',
     products: OrderedProductsMock,
   },
