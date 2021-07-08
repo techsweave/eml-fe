89497dad1b760e924cc8c1f5d1165eaffdbcc1b3
Nicolò Giaccone <giacco9722@outlook.it>
2021-07-08 18:14:37 +0200

    delete useless files

--- src/components/order/OrderItem.tsx
+++ src/components/order/OrderItem.tsx
@@ -119,7 +119,7 @@ const OrderItem = (prop: { order: Models.Tables.IOrder }) => {
                 </Td>
               </Tr>
               {state.map((item) => (
-                <Tr key={item.id}>
+                <Tr key={item.title}>
                   <Td><Image fallbackSrc="/images/fallback.png" src={item.imageURL} alt={item.title} maxWidth='250px' /></Td>
                   <Td>{item.title}</Td>
                   <Td>
