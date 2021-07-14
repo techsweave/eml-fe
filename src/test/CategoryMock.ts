e95999288fdefe56984aa28f7af1ece547c2d9ad
TitoScutari <titoscutari@gmail.com>
2021-07-14 18:05:01 +0200

    product detail visibility test

---
+++ src/test/CategoryMock.ts
@@ -0,0 +1,11 @@
+import { Models } from 'utilities-techsweave';
+
+const categoryMock: Models.Tables.ICategory = {
+  id: '123456789',
+  name: 'pantaloni',
+  macroCategorieId: '987654321',
+  description: 'ciò sono braghe',
+  taxes: 22,
+  // customSpecTemplates?: ISpecTemplate[]; what
+};
+export default categoryMock;
