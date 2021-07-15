994a98e27826b2d7768bda16c6d51dc7fa0ff90c
TitoScutari <titoscutari@gmail.com>
2021-07-15 16:07:07 +0200

    cart item test

---
+++ src/test/CartItem.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+import CartItem from '../components/cart/CartItem';
+import productMock from './ProductMock';
+
+describe('cart item test', () => {
+  let expectedItem;
+
+  beforeAll(() => {
+    // eslint-disable-next-line prefer-destructuring
+    expectedItem = productMock[0];
+  });
+
+  test('cartItem visibility', () => {
+    render(<CartItem cartItem={expectedItem} />);
+    expect(screen.getByText(expectedItem.title)).toBeVisible();
+    expect(screen.getByText(expectedItem.price, { exact: false })).toBeVisible();
+  });
+});
