e0ca854e31d62591c3d5cca36e1bdb5c967afab5
samupd2013 <samupd2013@gmail.com>
2021-07-21 18:33:50 +0200

    fix

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,7 @@
-import { Box, Stack, StackDivider, Divider } from '@chakra-ui/react'
-import * as React from 'react'
-import { Copyright } from './Copyright'
-import { LinkGrid } from './LinkGrid'
-import { Logo } from './Logo'
-import { SocialMediaLinks } from './SocialMediaLinks'
-import { SubscribeForm } from './SubscribeForm'
+import { Box, Stack, StackDivider, Divider } from '@chakra-ui/react';
+import * as React from 'react';
+import { Copyright } from './Copyright';
+import { SocialMediaLinks } from './SocialMediaLinks';
 const Footer = () => (
   <Box as="footer" role="contentinfo" mx="auto" py="12" px={{ base: '4', md: '8' }}>
     <Divider mb='10'/>
