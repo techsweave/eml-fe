b95737effc9e6db948503a138382fe6817a80878
Nicolò Giaccone <giacco9722@outlook.it>
2021-07-23 09:25:27 +0200

    fixes

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,7 @@
 import { Box, Stack, Divider } from '@chakra-ui/react';
 import * as React from 'react';
-import { Copyright } from './Copyright';
-import { SocialMediaLinks } from './SocialMediaLinks';
+import Copyright from './Copyright';
+import SocialMediaLinks from './SocialMediaLinks';
 
 const Footer = () => (
   <Box as="footer" role="contentinfo" mx="auto" py="12" px={{ base: '4', md: '8' }}>
