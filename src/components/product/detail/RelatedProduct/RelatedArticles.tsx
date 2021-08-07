d7c039f2fb45219869b9467bee5bc80a6a3ee885
Nicolò Giaccone <giacco9722@outlook.it>
2021-08-07 11:42:27 +0200

    layout refactor

--- src/components/product/detail/RelatedProduct/RelatedArticles.tsx
+++ src/components/product/detail/RelatedProduct/RelatedArticles.tsx
@@ -9,7 +9,7 @@ const RelatedProducts = (prop:{ product:Models.Tables.IProduct[] }) => {
   const { product } = prop;
   return (
     <Box mt='5'>
-      <Heading mt='10' mb='10'>Related Product</Heading>
+      <Heading mt='10' mb='10'>Alternative Product</Heading>
       {product.length === 0 && (<Text textAlign='center' fontSize='3xl'>There is no products corrisponding with this category</Text>)}
       {product.length !== 0 && (
       <Grid templateColumns={['repeat(2, 1fr)', 'repeat(2, 1fr)', 'repeat(6, 1fr)', 'repeat(6, 1fr)']} gap={10} ml={['5', '5', '0', '0']} mr={['5', '5', '10', '10']}>
