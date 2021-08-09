b953fc1e905a867e35e0d7ab4d7e105216d76d4a
Nicolò Giaccone <giacco9722@outlook.it>
2021-08-09 10:26:17 +0200

    fix last commit

--- src/components/home/Carousel/CarouselItem.tsx
+++ src/components/home/Carousel/CarouselItem.tsx
@@ -1,15 +1,37 @@
 import Link from 'next/link';
-import { Models } from 'utilities-techsweave';
-import React from 'react';
+import { AuthenticatedUser, Models } from 'utilities-techsweave';
+import React, { useEffect, useState } from 'react';
 import { Text } from '@chakra-ui/layout';
 import { Box, Image, Stack } from '@chakra-ui/react';
+import { useSession } from 'next-auth/client';
 
 const CarouselItem = (prop: { product: Models.Tables.IProduct }) => {
   const { product } = prop;
+  const session = useSession()[0];
+  const [userState, setState] = useState<boolean>();
+
+  async function isVendor(s) {
+    const user = await AuthenticatedUser.fromToken(s?.accessToken as string);
+    return user.isVendor(process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID!);
+  }
+  useEffect(() => {
+    const s = session;
+    if (userState !== undefined) return;
+    if (!s) return;
+    isVendor(s).then(
+      (data) => {
+        setState(data);
+      },
+    ).catch(
+      (err) => {
+        console.log(err.message);
+      },
+    );
+  }, [userState, setState, session]);
   const DiscountPrice = (product.price * ((100 - product.discount!) / 100)).toFixed(2);
   return (
     <Box as="button" w='100%'>
-      <Link href={{ pathname: '/products/detail/[id]', query: { id: product.id } }}>
+      <Link href={{ pathname: userState ? '/editProduct/[id]' : '/products/detail/[id]', query: { id: product.id } }}>
         <Stack position='relative'>
           <Image src={product.imageURL} fallbackSrc='/images/fallback.png' alt={product.title} w="100%" h={['300px', '300px', '600px', '600px']} borderRadius="15px" fit="scale-down" />
           <Text position='absolute' top='0' right='5' fontWeight='bold' color='red.500' fontSize='4xl'>
--- src/components/product/detail/RelatedProduct/RelatedItem.tsx
+++ src/components/product/detail/RelatedProduct/RelatedItem.tsx
@@ -1,37 +1,14 @@
 import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Text } from '@chakra-ui/layout';
 import { Box, Image, Stack } from '@chakra-ui/react';
-import { useSession } from 'next-auth/client';
-import { AuthenticatedUser } from 'utilities-techsweave';
 
 const ProductItem = (prop) => {
   const { product } = prop;
-  const session = useSession()[0];
-  const [userState, setState] = useState<boolean>();
-
-  async function isVendor(s) {
-    const user = await AuthenticatedUser.fromToken(s?.accessToken as string);
-    return user.isVendor(process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID!);
-  }
-  useEffect(() => {
-    const s = session;
-    if (userState !== undefined) return;
-    if (!s) return;
-    isVendor(s).then(
-      (data) => {
-        setState(data);
-      },
-    ).catch(
-      (err) => {
-        console.log(err.message);
-      },
-    );
-  }, [userState, setState, session]);
   const discountedPrice = (product.price * ((100 - product.discount!) / 100));
   return (
     <Box as="button" w='100%'>
-      <Link href={{ pathname: userState ? '/editProduct/[id]' : '/products/detail/[id]', query: { id: product.id } }}>
+      <Link href={{ pathname: '/products/detail/[id]', query: { id: product.id } }}>
         <Stack position='relative'>
           <Image src={product.imageURL} fallbackSrc="/images/fallback.png" alt={product.title} w="100%" h='150px' borderRadius="15px" fit="scale-down" />
           <Stack position='absolute' bottom='0' bg='rgba(44,44,44,0.7)' textColor='white' w='100%' h='40%' borderBottomRadius='15px' justifyContent='center'>
